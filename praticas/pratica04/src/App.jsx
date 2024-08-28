db371e547090d6e4add001ea87e6eeac5e9ec690
unknown <2314290130@ACADEMICO.IESB.BR>
2024-08-27 21:47:02 -0300

    adicionando codigos da pratica04

---
+++ praticas/pratica04/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './components/header';
+import './App.css'; 
+import './components/header.css'; 
+
+function App() {
+  return (
+   <div className="card">
+     <Header/>
+     <p>Este é um parágrafo em JSX!</p>
+   </div>
+  );
+}
+
+
+export default App;
